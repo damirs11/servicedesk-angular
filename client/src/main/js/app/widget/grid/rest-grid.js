98e0759c9ead91ba88fc800ee55d517fcfe3f264
quadrix <mail>
2017-06-22 02:47:55 +0500

    Разработка виджета для работы с таблицами...

--- client/src/main/js/app/widget/grid/rest-grid.js
+++ client/src/main/js/app/widget/grid/rest-grid.js
@@ -32,7 +32,6 @@
                     enableSorting: true,
                     excludeSortColumns: true,
                     enableRowSelection: true,
-                    //enableFullRowSelection: true,
                     enableSelectAll: true,
                     selectionRowHeaderWidth: 35,
                     multiSelect: true,
@@ -41,14 +40,10 @@
                     enableFiltering: true,
                     paginationPageSizes: [10, 20, 50, 100, 200, 500],
                     paginationPageSize: $scope.dataOptions.paging.pageSize,
+                    useExternalPagination : true,
                     columnDefs: $scope.dataOptions.metaData,
-                    data: [
-                        {id: 1, name: 'Вася', age: 15},
-                        {id: 2, name: 'Петя', age: 25},
-                        {id: 3, name: 'Андрей', age: 43},
-                        {id: 4, name: 'Коля', age: 62},
-                        {id: 5, name: 'Петр', age: 8}
-                    ],
+                    data: [],
+                    totalItems: 0,
                     rowTemplate: "<div ng-dblclick=\"grid.appScope.onDblClick(row)\" ng-repeat=\"(colRenderIndex, col) in colContainer.renderedColumns track by col.colDef.name\" class=\"ui-grid-cell\" ng-class=\"{ 'ui-grid-row-header-cell': col.isRowHeader }\" ui-grid-cell ></div>",
 
                     isRowSelectable: function (row) {
@@ -58,7 +53,7 @@
                     appScopeProvider: {
                         onDblClick: function (row) {
                             if (row.entity) {
-                                alert('dbl click ' + row.entity.name);
+                                alert('dbl click ' + row.entity.id);
                             }
                         }
                     },
@@ -66,36 +61,53 @@
                         //set gridApi on scope
                         $scope.gridApi = gridApi;
                         gridApi.core.on.sortChanged($scope, function (grid, sortColumns) {
+                            console.log('sortChanged');
                             $scope.dataOptions.sort = sortColumns;
-                            fetchData()
+                            fetchData();
                         });
                         gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+                            console.log('rowSelectionChanged');
                             //row.isSelected;
                             // Вызывается при выделении(снятии выделение) строки
                             //setCurrentRow($scope.gridApi.grid.selection.lastSelectedRow);
                         });
                         gridApi.selection.on.rowSelectionChangedBatch($scope, function(rows){
+                            console.log('rowSelectionChangedBatch');
                             //rows.length
                             // Вызывается когда выделяются(снимается выделение) все строки щелчком по заголовку таблицы
                             $scope.numberOfSelectedItems = rows.length;
                             //setCurrentRow($scope.gridApi.grid.selection.lastSelectedRow)
                         });
                         gridApi.pagination.on.paginationChanged($scope, function (pageNumber, pageSize) {
+                            console.log('paginationChanged');
                             if ($scope.dataOptions.paging.pageSize != pageSize) {
                                 // В случае изменения значения параметра "Кол-во записей на странице" переходим на 1-ю страницу
                                 $scope.dataOptions.paging.pageNumber = 1;
-                                $scope.gridOptions.paginationCurrentPage = 1
+                                $scope.gridOptions.paginationCurrentPage = 1;
+                                $scope.dataOptions.paging.pageSize = pageSize;
                             } else {
                                 $scope.dataOptions.paging.pageNumber = pageNumber
                             }
-                            $scope.dataOptions.paging.pageSize = pageSize;
-                            fetchData()
+                            fetchData();
                         });
                     }
                 };
                 var fetchData = function () {
-                    console.info('fetch data')
-                }
+                    var page = $scope.dataOptions.paging.pageNumber;
+                    var size = $scope.dataOptions.paging.pageSize;
+                    var rows = [];
+                    console.info('fetch data');
+                    for (var i = (page - 1) * size; i < page * size; i++) {
+                        rows.push({
+                            id: i,
+                            name: "Вася",
+                            age: Math.round((Math.random() * 40 + 10))
+                        })
+                    }
+                    $scope.gridOptions.data = rows;
+                    $scope.gridOptions.totalItems = 300;
+                };
+                fetchData();
             }]);
 
 }());
