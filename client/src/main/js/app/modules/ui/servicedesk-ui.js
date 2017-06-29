8d211045f2e39e8162a5143748112571fe2d24a9
MYXOMOPX <MYXOMOPX@gmail.com>
2017-06-29 16:21:51 +0500

    Убрал лишние файлы, оставшиеся после рефакторинга

--- client/src/main/js/app/modules/ui/servicedesk-ui.js
+++ client/src/main/js/app/modules/ui/servicedesk-ui.js
@@ -1,7 +1,6 @@
 import {servicedeskAPI} from "../api/servicedesk-api"
 import {StateConfig} from "./state/StateConfig"
 import {ModalConfig} from "./modal/ModalConfig"
-import {uiDialogs} from "../uiDialogs/dialogs";
 import {uiRouter, translate, uiBootstrap, uiGrid, ngSanitize, utilsEntity, ngMessages} from "../../utils/web-libraries"
 import {TranslateConfig} from "./translate/translate.config"
 import {default as ModalAction} from "../ModalAction/ModalAction"
