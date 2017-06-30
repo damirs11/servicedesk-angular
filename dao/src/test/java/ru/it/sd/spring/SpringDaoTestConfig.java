5bd06f91211326864ab5c9ce53b77cd8344966b3
Marat Fayzullin <mfayzullin@aplana.com>
2017-06-30 21:45:43 +0500

    Починил сборку клиентского модуля из gradle

--- dao/src/test/java/ru/it/sd/spring/SpringDaoTestConfig.java
+++ dao/src/test/java/ru/it/sd/spring/SpringDaoTestConfig.java
@@ -1,6 +1,5 @@
 package ru.it.sd.spring;
 
-import com.jolbox.bonecp.BoneCPDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
