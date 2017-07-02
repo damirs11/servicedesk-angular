cdcbf10b43f201071e76ccdccc054c9cf734750d
MYXOMOPX <MYXOMOPX@gmail.com>
2017-07-02 14:36:51 +0500

    Поправил бин WebAccount. Убрал лишний модуль из зависимостей npm. Убрал установку gulp с помощью gradle, этим занимается npm. Вернул старые сообщения в messages.properties

--- service/src/main/java/ru/it/sd/service/WebAccountBean.java
+++ service/src/main/java/ru/it/sd/service/WebAccountBean.java
@@ -3,10 +3,13 @@ package ru.it.sd.service;
 import com.hp.itsm.api.ApiSDSession;
 import com.hp.itsm.ssp.beans.SdClientBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by MYXOMOPX on 030 30.06.17.
@@ -15,13 +18,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class WebAccountBean {
     private SdClientBean sdClient;
-    @Autowired
-    private Environment env;
 
-    public WebAccountBean(){
-        String server = env.getProperty("sd_application_server");
-        String login = env.getProperty("sd_dummy_login");
-        String password = env.getProperty("sd_dummy_password");
+    @Value("${sd_application_server}")
+    private String server;
+    @Value("${sd_dummy_login}")
+    private String login;
+    @Value("${sd_dummy_password}")
+    private String password;
+
+    @PostConstruct
+    private void createClient(){
         sdClient = new SdClientBean(server, login, password);
     }
 
