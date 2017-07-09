6652a4ef31aedf70a7ffd537115f58d615621655
quadrix <mail>
2017-07-09 21:38:17 +0500

    Поправил стейт для перехода на страницу ввода пароля

--- client/src/main/js/ui/components/auth.transition.js
+++ client/src/main/js/ui/components/auth.transition.js
@@ -4,7 +4,7 @@ function AuthTransition($transitions,SD,$state){
     $transitions.onBefore({
         to: state => state.data && state.data.needAuthorize && !SD.authorized
     },(transition) => {
-        return $state.target("apps.login")
+        return $state.target("app.login")
     });
 }
 
