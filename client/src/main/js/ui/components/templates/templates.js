a8eb90c489db55e390a4001b9b8a9809dde3b219
MYXOMOPX <MYXOMOPX@gmail.com>
2017-09-17 16:47:51 +0500

    Добавил столбец "Просрочено"

--- client/src/main/js/ui/components/templates/templates.js
+++ client/src/main/js/ui/components/templates/templates.js
@@ -2,12 +2,14 @@
 import errorMessages from "./messages.html";
 import priorityCell from "./cells/priority.cell.html"
 import personCell from "./cells/person.cell.html"
+import expiredCell from "./cells/expired.cell.html"
 
 Templates.$inject = ["$templateCache"];
 function Templates($templateCache){
     $templateCache.put("template.ngMessage.errorMessages", errorMessages);
     $templateCache.put("template.grid.cell.priority", priorityCell);
     $templateCache.put("template.grid.cell.person", personCell);
+    $templateCache.put("template.grid.cell.expired", expiredCell);
 
 }
 
