6531ab721e4ff5b35e438c4b835686342fd9127a
quadrix <mail>
2017-09-25 02:23:15 +0500

    Небольшие правки стилей

--- client/src/main/js/ui/components/templates/templates.js
+++ client/src/main/js/ui/components/templates/templates.js
@@ -1,7 +1,10 @@
 // Шаблон стандартных сообщений для валидаторов полей ввода
 import errorMessages from "./messages.html";
+// Приоритет
 import priorityCell from "./cells/priority.cell.html"
+// Персона
 import personCell from "./cells/person.cell.html"
+// Просрочено? Да-Нет
 import expiredCell from "./cells/expired.cell.html"
 
 Templates.$inject = ["$templateCache"];
@@ -10,7 +13,6 @@ function Templates($templateCache){
     $templateCache.put("template.grid.cell.priority", priorityCell);
     $templateCache.put("template.grid.cell.person", personCell);
     $templateCache.put("template.grid.cell.expired", expiredCell);
-
 }
 
 export {Templates};
