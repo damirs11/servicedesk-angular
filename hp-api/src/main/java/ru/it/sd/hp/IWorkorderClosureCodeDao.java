993bb30f3f5662cae5e0d017194cbb1c8efe8a99
Nikita Sychev <stem25@it.ru>
2018-05-10 18:30:38 +0500

    Исправил ошибку hp-api

--- hp-api/src/main/java/ru/it/sd/hp/IWorkorderClosureCodeDao.java
+++ hp-api/src/main/java/ru/it/sd/hp/IWorkorderClosureCodeDao.java
@@ -1,37 +1,38 @@
 package ru.it.sd.hp;
 
-import com.hp.itsm.api.interfaces.IChangeCode1;
+import com.hp.itsm.api.interfaces.IChangeClosureCode;
+import com.hp.itsm.api.interfaces.IWorkorderClosureCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.it.sd.exception.ServiceException;
-import ru.it.sd.model.EntityCode1;
+import ru.it.sd.model.EntityClosureCode;
 
 /**
  * Created by nsyhev 29.09.2017
  */
 @Repository
-public class IChangeCode1Dao implements HpCrudDao<EntityCode1, IChangeCode1> {
+public class IWorkorderClosureCodeDao implements HpCrudDao<EntityClosureCode, IWorkorderClosureCode> {
 
     @Autowired
     private HpApi api;
 
 
     @Override
-    public long create(EntityCode1 entity) {
+    public long create(EntityClosureCode entity) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public IChangeCode1 read(long id) {
+    public IWorkorderClosureCode read(long id) {
         try{
-            return api.getSdClient().sd_session().getChangeCode1Home().openChangeCode1(id);
+            return api.getSdClient().sd_session().getWorkorderClosureCodeHome().openWorkorderClosureCode(id);
         }catch (Exception e){
-            throw new ServiceException("Не найден", e);
+            throw new ServiceException("Не найден ", e);
         }
     }
 
     @Override
-    public void update(EntityCode1 entity) {
+    public void update(EntityClosureCode entity) {
         throw new UnsupportedOperationException();
     }
 
