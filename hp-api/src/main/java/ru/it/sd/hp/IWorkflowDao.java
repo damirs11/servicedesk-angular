ac93b2c73276df9c4936af4aeeb7fd611ebdfea5
Никита Сычев <nsychev@it.ru>
2018-12-12 15:27:57 +0500

    Включил согласования для "Проблем"

--- hp-api/src/main/java/ru/it/sd/hp/IWorkflowDao.java
+++ hp-api/src/main/java/ru/it/sd/hp/IWorkflowDao.java
@@ -4,6 +4,7 @@ import com.hp.itsm.api.interfaces.IWorkflow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.it.sd.hp.change.IChangeDao;
+import ru.it.sd.hp.problem.IProblemDao;
 import ru.it.sd.hp.workorder.IWorkorderDao;
 import ru.it.sd.model.EntityType;
 
@@ -11,26 +12,27 @@ import ru.it.sd.model.EntityType;
  * Created by user on 27.07.2017.
  */
 @Repository
-public class IWorkflowDao  {
+public class IWorkflowDao {
+
+    private final IChangeDao iChangeDao;
+    private final IWorkorderDao iWorkorderDao;
+    private final IProblemDao iProblemDao;
 
     @Autowired
-    private IChangeDao iChangeDao;
-    @Autowired
-    private IWorkorderDao iWorkorderDao;
+    public IWorkflowDao(IChangeDao iChangeDao, IWorkorderDao iWorkorderDao, IProblemDao iProblemDao) {
+        this.iChangeDao = iChangeDao;
+        this.iWorkorderDao = iWorkorderDao;
+        this.iProblemDao = iProblemDao;
+    }
 
     public IWorkflow read(long id, EntityType entityType) {
 
-        if(entityType == null) throw new IllegalArgumentException("Не указан тип сущности");
-        switch (entityType){
-            case CHANGE:{
-                return iChangeDao.read(id);
-            }
-            case WORKORDER:{
-                return iWorkorderDao.read(id);
-            }
-            default:{
-                throw new IllegalArgumentException("Не правильно указан тип сущности");
-            }
+        if (entityType == null) throw new IllegalArgumentException("Не указан тип сущности");
+        switch (entityType) {
+            case CHANGE: return iChangeDao.read(id);
+            case WORKORDER: return iWorkorderDao.read(id);
+            case PROBLEM: return iProblemDao.read(id);
+            default: throw new IllegalArgumentException("Не правильно указан тип сущности");
         }
 
 
