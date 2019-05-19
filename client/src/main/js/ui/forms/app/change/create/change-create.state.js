d426b84e41c850197ef210b65c79a4afadec77f3
Файзуллин Марат <mfayzullin@it.ru>
2019-05-20 03:20:14 +0500

    Изменения: реализовал работу без кэша

--- client/src/main/js/ui/forms/app/change/create/change-create.state.js
+++ client/src/main/js/ui/forms/app/change/create/change-create.state.js
@@ -1,8 +1,6 @@
 import template from "./change-create.html"
 import {controller} from "./change-create.ctrl"
 import {SDResolver} from "../../sd.resolver";
-import {NewChangeResolver} from "./new-change-resolver";
-import {PassedParamsResolver} from "./passed-arguments-resolver";
 
 const ChangeCreateState = {
     name: "app.change.create",
@@ -20,5 +18,24 @@ const ChangeCreateState = {
         passedParams: PassedParamsResolver
     }
 };
+
+// Новое изменение внедряется зависимостью.
+// Все для того, чтобы передавать без проблем его между дочерними стейтами.
+NewChangeResolver.$inject = ["SD"];
+function NewChangeResolver(SD) {
+    const change = new SD.Change();
+    change.$update({
+        assignment: new SD.EntityAssignment(),
+    });
+    return change;
+}
+
+PassedParamsResolver.$inject = ["$stateParams"];
+function PassedParamsResolver($stateParams){
+    const passedParams = {
+        templateId: $stateParams.templateId || undefined,
+    };
+    return passedParams
+}
 
 export {ChangeCreateState}
--- client/src/main/js/ui/forms/app/change/create/passed-arguments-resolver.js
+++
@@ -1,8 +0,0 @@
-function PassedParamsResolver($stateParams){
-    const passedParams = {
-        templateId: $stateParams.templateId || undefined,
-    };
-    return passedParams
-}
-PassedParamsResolver.$inject = ["$stateParams"];
-export {PassedParamsResolver};
